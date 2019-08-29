b9ccaca7096c71b37786c0e3945cfa88d3cda8d7
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-08-29 21:28:45 +0100

    added uri validation

--- src/wamp_uri.erl
+++ src/wamp_uri.erl
@@ -10,40 +10,44 @@
 -module (wamp_uri).
 -include("wamp.hrl").
 
--export([parse/1]).
+-type rule()    ::  loose | loose_allow_empty | strict | strict_allow_empty.
+
+-export_type([rule/0]).
+
 -export([is_valid/1]).
+-export([is_valid/2]).
 -export([components/1]).
 
-%% URI components SHOULD only contain letters, digits and "_".
-%% and allow empty uri components
-%% re.compile("^(([0-9a-z_]+\.)|\.)*([0-9a-z_]+)?$")
 
-%% Further, application URIs MUST NOT use "wamp" as a first URI
-%% component, since this is reserved for URIs predefined with the WAMP
-%% protocol itself.
 
-%% The URIs are understood to form a single, global, hierarchical
-%% namespace for WAMP.
-%%
-%% The namespace is unified for topics, procedures and errors - these
-%% different resource types do NOT have separate namespaces.
+%% =============================================================================
+%% API
+%% =============================================================================
 
-%% Further, application URIs MUST NOT use "wamp" as a first URI
-%% component, since this is reserved for URIs predefined with the WAMP
-%% protocol itself.
 
-parse(URI) when is_list(URI) ->
-    %% TODO
-    {ok, URI};
-parse(URI) when is_binary(URI) ->
-    %% TODO
-    {ok, URI}.
+
+%% -----------------------------------------------------------------------------
+%% @doc Sames as `is_valid(Uri, loose_allow_empty)'.
+%% @end
+%% -----------------------------------------------------------------------------
+-spec is_valid(uri()) -> boolean().
 
 is_valid(Uri) ->
-    case catch parse(Uri) of
-        {ok, _} -> true;
-        _ -> false
-    end.
+    is_valid(Uri, loose_allow_empty).
+
+
+
+%% -----------------------------------------------------------------------------
+%% @doc
+%% @end
+%% -----------------------------------------------------------------------------
+-spec is_valid(Uri :: uri(), Rule :: rule()) -> boolean().
+
+is_valid(Uri, Rule) when is_binary(Uri) ->
+    re:run(Uri, uri_pattern(Rule)) =/= nomatch;
+
+is_valid(_, _) ->
+    false.
 
 
 %% -----------------------------------------------------------------------------
@@ -54,6 +58,7 @@ is_valid(Uri) ->
 %% @end
 %% -----------------------------------------------------------------------------
 -spec components(uri()) -> [binary()] | no_return().
+
 components(Uri) ->
     case binary:split(Uri, <<".">>, [global]) of
         [TopLevelDomain, AppName | Rest] when length(Rest) > 0 ->
@@ -63,3 +68,41 @@ components(Uri) ->
             %% Invalid Uri
             error({?WAMP_INVALID_URI, Uri})
     end.
+
+
+
+%% =============================================================================
+%% PRIVATE
+%% =============================================================================
+
+
+
+%% @private
+uri_pattern(Rule) ->
+    CompiledPattern = persistent_term:get({?MODULE, Rule}, undefined),
+    uri_pattern(Rule, CompiledPattern).
+
+
+%% @private
+uri_pattern(loose = Rule, undefined) ->
+    {ok, Pattern} = re:compile("^([^\s\.#]+\.)*([^\s\.#]+)$"),
+    ok = persistent_term:put({?MODULE, Rule}, Pattern),
+    Pattern;
+
+uri_pattern(loose_allow_empty = Rule, undefined) ->
+    {ok, Pattern} = re:compile("^(([^\s\.#]+\.)|\.)*([^\s\.#]+)?$"),
+    ok = persistent_term:put({?MODULE, Rule}, Pattern),
+    Pattern;
+
+uri_pattern(strict = Rule, undefined) ->
+    {ok, Pattern} = re:compile("^([0-9a-z_]+\.)*([0-9a-z_]+)$"),
+    ok = persistent_term:put({?MODULE, Rule}, Pattern),
+    Pattern;
+
+uri_pattern(strict_allow_empty = Rule, undefined) ->
+    {ok, Pattern} = re:compile("^(([0-9a-z_]+\.)|\.)*([0-9a-z_]+)?$"),
+    ok = persistent_term:put({?MODULE, Rule}, Pattern),
+    Pattern;
+
+uri_pattern(_, CompiledPattern) ->
+    CompiledPattern.
